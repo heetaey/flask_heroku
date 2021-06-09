1acbe610f52ec95637b6ad87a78c98e6265e4459
heetaeyang <htyang90@gmail.com>
2021-06-09 16:47:23 -0700

    code cleanup

--- resources/user.py
+++ resources/user.py
@@ -5,16 +5,16 @@ from models.user import UserModel
 class UserRegister(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('username',
-        type = str,
-        required = True,
-        help = "This field cannot be left blank!"
-    )
+                        type = str,
+                        required = True,
+                        help = "This field cannot be left blank!"
+                        )
 
     parser.add_argument('password',
-        type = str,
-        required = True,
-        help = "This field cannot be left blank!"
-    )
+                        type = str,
+                        required = True,
+                        help = "This field cannot be left blank!"
+                        )
 
 
     def post(self):
