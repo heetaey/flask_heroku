a8f3575568e529f2a0c2dba06195f32be68cdde9
heetaeyang <htyang90@gmail.com>
2021-06-14 13:22:19 -0700

    implements jwt identity via jwt-optional

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt
+from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity
 from models.item import ItemModel
 
 
@@ -69,5 +69,14 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_required(optional=True)
     def get(self):
-        return {'items': [item.json() for item in ItemModel.find_all()]}
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        
+        return {
+                'items': [item['name'] for item in items],
+                'message': 'More data available if you are logged in.'
+            }, 200
