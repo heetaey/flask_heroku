203dc5a573339610f52481b07cbae305e1189036
heetaeyang <htyang90@gmail.com>
2021-06-14 13:36:24 -0700

    implements token refresh in the REST

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
-from resources.user import UserRegister, User, UserLogin
+from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
@@ -45,6 +45,7 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
     db.init_app(app)
--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,11 @@
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+                            create_access_token, 
+                            create_refresh_token,
+                            jwt_required,
+                            get_jwt_identity
+                        )
 from models.user import UserModel
 
 _user_parser = reqparse.RequestParser()
@@ -63,3 +68,11 @@ class UserLogin(Resource):
             }, 200
 
         return {'message': 'Invalid credentials'}, 401
+
+
+class TokenRefresh(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        curr_user = get_jwt_identity()
+        new_token = create_access_token(identity=curr_user, fresh=False)
+        return {'access_token': new_token}, 200
